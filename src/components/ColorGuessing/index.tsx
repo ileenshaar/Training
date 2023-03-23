5e724c220198d23cdbad01596734142b5b9dd465
ileen-Shaar <i26203332@gmail.com>
2023-03-23 13:14:38 +0200

    color guessing-update1

---
+++ src/components/ColorGuessing/index.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import Colors from './Colors.'
+import './index.css'
+
+export const ColorGuessing = () => {
+  const [currentColor, setCurrentColor] = useState(Colors[0])
+  const [buttonColors, setButtonColors] = useState(
+    getButtonColors(Colors, currentColor)
+  )
+  const [count, setCount] = useState(0)
+
+  function getButtonColors(
+    Colors: { name: string; hex: string }[],
+    currentColor: { name: string; hex: string }
+  ) {
+    const otherColors = Colors.filter(color => color.hex !== currentColor.hex)
+
+    const shuffledColors = otherColors.sort(() => 0.5 - Math.random())
+
+    const buttonColors = shuffledColors.slice(0, 2)
+
+    const allButtonColors = [currentColor, ...buttonColors].sort(
+      () => 0.5 - Math.random()
+    )
+
+    return allButtonColors
+  }
+
+  function handleButtonClick(color: string) {
+    setCount(count + 1)
+    if (color === currentColor.hex) {
+      alert('You guessed correctly!')
+    } else {
+      alert('Sorry, that is incorrect.')
+    }
+    // Change the current color and button colors
+    if (count < 5) {
+      const newColor = Colors[Math.floor(Math.random() * Colors.length)]
+      setCurrentColor(newColor)
+      setButtonColors(getButtonColors(Colors, newColor))
+    }
+  }
+
+  return (
+    <div>
+      <div
+        className="color-box"
+        style={{ backgroundColor: currentColor.hex }}
+      ></div>
+      <div className="button-container">
+        {buttonColors.map(color => (
+          <button
+            key={color.hex}
+            className="color-button"
+            onClick={() => handleButtonClick(color.hex)}
+          >
+            {color.hex}
+          </button>
+        ))}
+      </div>
+    </div>
+  )
+}
+export default ColorGuessing
