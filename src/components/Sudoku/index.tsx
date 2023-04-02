fcea2ca0f6c79bd05f07b85caa787dd47f5f351f
ileen-Shaar <i26203332@gmail.com>
2023-04-02 21:53:09 +0300

    Sudoku update

--- src/components/Sudoku/index.tsx
+++ src/components/Sudoku/index.tsx
@@ -1,12 +1,89 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './index.css'
+import axios from 'axios'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const sudokoLibrary = require('sudoku')
 
 const Sudoku = () => {
+  const [board, setBoard] = useState(
+    Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => '.'))
+  )
+
+  const [solution, setSolution] = useState('')
+
+  useEffect(() => {
+    const puzzle = sudokoLibrary.makepuzzle()
+
+    const newBoard = board.map((row, rowIndex) => {
+      return row.map((_, cellIndex) => {
+        const index = rowIndex * 9 + cellIndex
+        return puzzle[index] || '.'
+      })
+    })
+    setBoard(newBoard)
+  }, [])
+
+  const handleOnChange = (
+    rowIndex: number,
+    cellIndex: number,
+    value: string
+  ) => {
+    setBoard(prevBoard => {
+      const newBoard = prevBoard.map((row, rIndex) => {
+        if (rIndex === rowIndex) {
+          return row.map((cell, cIndex) => {
+            if (cIndex === cellIndex) {
+              return value
+            }
+            return cell
+          })
+        }
+        return row
+      })
+      return newBoard
+    })
+  }
+
+  const handleSubmit = () => {
+    const puzzle = board.map(row => row.join('')).join('')
+    axios
+      .post('http://127.0.0.1:5000/', {
+        sudoku: [puzzle]
+      })
+      .then(response => {
+        setSolution(response.data.solution)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
   return (
-    <div className="sudoku-grid">
-      {[...Array(81)].map((_, i) => (
-        <input key={i} type="text" maxLength={1} />
-      ))}
+    <div>
+      <div
+        className="sudoku-grid"
+        style={{ display: 'flex', flexDirection: 'column' }}
+      >
+        {board.map((row, rowIndex) => (
+          <div key={rowIndex} className="adjacent" style={{ display: 'flex' }}>
+            {row.map((cell, cellIndex) => (
+              <input
+                key={cellIndex}
+                type="text"
+                maxLength={1}
+                value={cell === '.' ? '' : cell}
+                onChange={e =>
+                  handleOnChange(rowIndex, cellIndex, e.target.value)
+                }
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+
+      <button onClick={() => handleSubmit()} className="submitButton">
+        submit
+      </button>
     </div>
   )
 }
