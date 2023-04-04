ee7d586db8e1e25f8bac50730250fb5bed289e98
ileen-Shaar <i26203332@gmail.com>
2023-04-05 00:33:23 +0300

    Sudoku-update

--- src/components/Sudoku/index.tsx
+++ src/components/Sudoku/index.tsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import './style.css'
 import axios from 'axios'
+import generatePuzzle from './generatePuzzle'
 
 const Sudoku = () => {
   const [board, setBoard] = useState(
     Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => '.'))
   )
-
-  const [solution, setSolution] = useState('')
 
   const handleOnChange = (
     rowIndex: number,
@@ -32,103 +31,24 @@ const Sudoku = () => {
 
   const handleSubmit = () => {
     const puzzle = board.map(row => row.join('')).join('')
-
+    console.log(puzzle)
     axios
       .post('http://127.0.0.1:5000', {
         sudoku: [puzzle]
       })
       .then(response => {
-        setSolution(response.data.data[0].solution)
+        const solution = response.data.data[0].solution
+        const newBoard = board.map((row, rowIndex) => {
+          return row.map((_, cellIndex) => {
+            const index = rowIndex * 9 + cellIndex
+            return solution[index]
+          })
+        })
+        setBoard(newBoard)
       })
       .catch(error => {
         console.error(error)
       })
-  }
-
-  const generatePuzzle = () => {
-    const generatedPuzzle = Array(81).fill(null)
-    for (let i = 0; i < 9; i += 3) {
-      const values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-      shuffleArray(values)
-      for (let j = 0; j < 3; j++) {
-        for (let k = 0; k < 3; k++) {
-          const index = (i + j) * 9 + (i + k)
-          generatedPuzzle[index] = values[j * 3 + k]
-        }
-      }
-    }
-
-    for (let i = 0; i < generatedPuzzle.length; i++) {
-      if (generatedPuzzle[i] === null) {
-        let randomNumber = Math.floor(Math.random() * 9) + 1
-        while (!checkBox(i, randomNumber)) {
-          randomNumber = Math.floor(Math.random() * 9) + 1
-        }
-        checkColumn(i, randomNumber)
-          ? checkRow(i, randomNumber)
-            ? (generatedPuzzle[i] = randomNumber)
-            : null
-          : null
-      }
-    }
-
-    const K = 20
-    removeElements(generatedPuzzle, K)
-
-    function removeElements(generatedPuzzle: number[], K: number): void {
-      const row = Math.floor(Math.random() * 81) + 1
-
-      if (generatedPuzzle[row] !== 0) {
-        K--
-        generatedPuzzle[row] = 0
-      }
-      K > 0 ? removeElements(generatedPuzzle, K) : null
-    }
-
-    function checkRow(index: number, randomNumber: number) {
-      const row = Math.floor(index / 9)
-
-      for (let i = 0; i < 9; i++) {
-        if (randomNumber == generatedPuzzle[row * 9 + i]) {
-          return false
-        }
-      }
-      return true
-    }
-
-    function checkColumn(index: number, randomNumber: number) {
-      const column = index % 9
-      for (let i = 0; i < 9; i++) {
-        const cellIndex = column + i * 9
-        if (randomNumber === generatedPuzzle[cellIndex]) {
-          return false
-        }
-      }
-      return true
-    }
-
-    function checkBox(index: number, randomNumber: number) {
-      const rowStart = Math.floor(Math.floor(index / 9) / 3) * 3
-      const columnStart = Math.floor((index % 9) / 3) * 3
-      for (let row = rowStart; row < rowStart + 3; row++) {
-        for (let column = columnStart; column < columnStart + 3; column++) {
-          const cellIndex = row * 9 + column
-          if (randomNumber === generatedPuzzle[cellIndex]) {
-            return false
-          }
-        }
-      }
-      return true
-    }
-
-    function shuffleArray(arr: number[]) {
-      for (let i = arr.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1))
-        ;[arr[i], arr[j]] = [arr[j], arr[i]]
-      }
-    }
-
-    return generatedPuzzle
   }
 
   useEffect(() => {
