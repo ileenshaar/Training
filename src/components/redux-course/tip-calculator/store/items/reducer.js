6a4c15131a924bd753f00e84911786e44b684b4a
ileen-Shaar <i26203332@gmail.com>
2023-04-24 04:00:42 +0300

    immer 2

--- src/components/redux-course/tip-calculator/store/items/reducer.js
+++ src/components/redux-course/tip-calculator/store/items/reducer.js
@@ -14,21 +14,16 @@ export const initialItems = [
   { uuid: id++, name: 'Vegan Ham mmm', price: 12, quantity: 1 }
 ]
 
-export const reducer = (state = initialItems, action) => {
-  //takes a func that will take the object you want to mmutate,
-  //give a draft version of it so you can mutate,
-  //then it will figure out how to immutably do all the changes
+export const reducer = produce((state = initialItems, action) => {
   if (action.type === ITEM_ADDED) {
-    return produce(state, draftState => {
-      const item = {
-        uuid: id++,
-        quantity: 1,
-        name: action.payload.name,
-        price: parseInt(action.payload.price, 10)
-      }
+    const item = {
+      uuid: id++,
+      quantity: 1,
+      name: action.payload.name,
+      price: parseInt(action.payload.price, 10)
+    }
 
-      draftState.push(item)
-    })
+    state.push(item)
   }
 
   if (action.type === ITEM_REMOVED) {
@@ -36,20 +31,14 @@ export const reducer = (state = initialItems, action) => {
   }
 
   if (action.type === ITEM_PRICE_UPDATED) {
-    return produce(state, draftState => {
-      const item = draftState.find(item => item.uuid === action.payload.uuid)
-      item.price = parseInt(action.payload.price, 10)
-    })
+    const item = state.find(item => item.uuid === action.payload.uuid)
+    item.price = parseInt(action.payload.price, 10)
   }
 
   if (action.type === ITEM_QUANTITY_UPDATED) {
-    return produce(state, draftState => {
-      const item = draftState.find(item => item.uuid === action.payload.uuid)
-      item.quantity = parseInt(action.payload.quantity, 10)
-    })
+    const item = state.find(item => item.uuid === action.payload.uuid)
+    item.quantity = parseInt(action.payload.quantity, 10)
   }
-
-  return state
-}
+}, initialItems)
 
 export default reducer
